3e35b1431226553567ecd90ddf2b50d0178a9802
Tom Merchant <me@tmerchant.com>
2019-11-06 15:19:42 +0000

    Created TestStockItem skeleton

---
+++ stockcontrol/TestStockItem.java
@@ -0,0 +1,15 @@
+
+package stockcontrol;
+
+/**
+* Tests the implementation of the StockItem Class
+*/
+class TestStockItem
+{
+
+    public static void main(String[] args)
+    {
+        /* TODO: Test cases go here */
+    }
+}
+
