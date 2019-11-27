a3521638348b993cbb0f906928bf16c81237f7b1
g5-adams <g5-adams@STU-CZC91081LQ.campus.ads.uwe.ac.uk>
2019-11-27 13:06:52 +0000

    added comments

--- stockcontrol/TestStockItem.java
+++ stockcontrol/TestStockItem.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 class TestResult
 {
+    //Declaring Variables
     static int testsPassed = 0;
     static int testsFailed = 0;
     boolean passed = false;
     String message;
     String name;
     
+    //Method for recording how many test passed
     public TestResult(String name, boolean passed, String message)
     {
         this.name = name;
@@ -42,6 +44,10 @@ class TestStockItem
         {
             case 0:
                 /* Test 1 */
+                /*
+                Testing the addStock Method by adding two stock 
+                then returning the new stock.
+                */
                 name = "Verifying StockItem.addStock() base case";
                 System.out.println(name);
                 testItem = new StockItem(1, 1, "test");
@@ -52,6 +58,10 @@ class TestStockItem
                 break;
             case 1:
                 /* Test 2 */
+                /*
+                Testing if adding trying have more than 100 total stock 
+                returns a error by adding 2 stock to 99.
+                */
                 input = new Scanner(System.in);
                 name = "Verifying StockItem.addStock() new stock exceeds 100";
                 System.out.println(name);
@@ -65,6 +75,10 @@ class TestStockItem
                 break;
             case 2:
                /* Test 3 */
+                /*
+                Testing than 100 stock is valid by adding for an exact total
+                of 100 stock.
+                */
                 name = "Verifying StockItem.addStock() new stock equals 100";
                 System.out.println(name);
                 testItem = new StockItem(99, 1, "test");
@@ -75,6 +89,10 @@ class TestStockItem
                 break;
             case 3:
                 /* Test 4 */
+                /*
+                Testing that adding negative stock returns an error by
+                trying to add -1 stock, and verifying the error.
+                */
                 input = new Scanner(System.in);
                 name = "Verifying StockItem.addStock() adding negative stock";
                 System.out.println(name);
@@ -88,6 +106,9 @@ class TestStockItem
                 break;
             case 4:
                 /* Test 5 */
+                /*
+                Testing that the sell stock method work by seling 2 stock.
+                */
                 name = "Verifying StockItem.sellStock() base case";
                 System.out.println(name);
                 testItem = new StockItem(3, 1, "test");
@@ -99,6 +120,11 @@ class TestStockItem
                 break;
             case 5:
                 /* Test 6 */
+                /*
+                Testing that you can't sell more stock then you have by 
+                trying to sell 2 stock with only 1 quantity, 
+                and verifying the error.
+                */
                 input = new Scanner(System.in);
                 name = "Verifying StockItem.sellStock() not enough stock";
                 System.out.println(name);
@@ -111,6 +137,10 @@ class TestStockItem
                 break;
             case 6:
                 /* Test 7 */
+                /*
+                Testing that can't sell negative stock by trying to sell 
+                -1 stock, and verifying the error.
+                */
                 input = new Scanner(System.in);
                 name = "Verifying StockItem.sellStock() selling negative stock";
                 System.out.println(name);
@@ -123,6 +153,9 @@ class TestStockItem
                 break;
             case 7:
                 /* Test 8 */
+                /*
+                Testing that VAT returns the correct value.
+                */
                 name = "Verifying StockItem.getVAT()";
                 System.out.println(name);
                 double VAT = StockItem.getVAT();
@@ -132,6 +165,10 @@ class TestStockItem
                 break;
             case 8:
                 /* Test 9 */
+                /*
+                Testing that to String returns to correct information by external
+                verification.
+                */
                 input = new Scanner(System.in);
                 name = "Verifying StockItem.toString()";
                 System.out.println(name);
@@ -144,6 +181,9 @@ class TestStockItem
                 break;
             case 9:
                 /* Test 10 */
+                /*
+                Testing that get price returns the price that was set.
+                */
                 name = "Verifying StockItem.getPrice()";
                 System.out.println(name);
                 testItem = new StockItem(1, 4.85f, "test");
@@ -153,6 +193,10 @@ class TestStockItem
                 break;
             case 10:
                 /* Test 11 */
+                /*
+                Testing that getVatPrice correct 
+                applys the Vat to the price and rounds for a valid price.
+                */
                 name = "Verifying StockItem.getVATPrice()";
                 System.out.println(name);
                 testItem = new StockItem(1, 4.85f, "test");
@@ -162,14 +206,19 @@ class TestStockItem
                 break;
             case 11:
                 /* Test 12 */
+                /*
+                Testing that set price works by correctly changing the price
+                from 9.99 to 8.88.
+                */
                 name = "Verifying StockItem.setPrice()";
                 System.out.println(name);
-                testItem = new StockItem(1, 8.88f, "test");
+                testItem = new StockItem(1, 9.99f, "test");
                 testItem.setPrice(8.88f);
                 message = (passed = testItem.getPrice() == 8.88f) ?
                     "setPrice is working"
                     : "setPrice is not working, expected 8.88, got " + testItem.getPrice();
                 break;
+                // Defult case.
             default:
                 passed = false;
                 name    = "Unimplemented test";
@@ -177,7 +226,7 @@ class TestStockItem
                 message = "Test hasn't been implemented";
                 break;
         }
-
+        //Prints the result of each test.
         System.out.println("Test " + (passed ? "passed" : "failed"));
         System.out.println();
         return new TestResult(name, passed, message);
@@ -185,13 +234,16 @@ class TestStockItem
 
     public static void main(String[] args)
     {
+        //Loops though each test.
         int NUMTESTS = 12, i;
         TestResult[] results = new TestResult[NUMTESTS];
-
+                
         for(i = 0; i < NUMTESTS; i++)
         {
             results[i] = runTest(i);
         }
+        
+        //Prints results for then entire test.
         System.out.printf("%d tests run, %d tests passed, %d tests failed\n",
                           NUMTESTS, TestResult.testsPassed, TestResult.testsFailed);
     }
