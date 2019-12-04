130b6e4d92deaaa4f0725c6ce61b658e87f53272
Tom Merchant <me@tmerchant.com>
2019-12-04 11:06:24 +0000

    TestPolymorphism class skeleton

--- stockcontrol/TestPolymorphism.java
+++ stockcontrol/TestPolymorphism.java
@@ -1,5 +1,14 @@
+package stockcontrol;
 
 public class TestPolymorphism
 {
+    private static void itemInstance(StockItem testItem)
+    {
 
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
 }
